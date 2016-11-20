786d4afbfcb6a6e1d205ab60b40f21ce9bcc4d46
Robert Stiff <uatecuk@gmail.com>
2016-11-20 23:44:50 +0000

    Experiment with dynamic proxy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Castle.DynamicProxy;
 
 namespace ConsoleApplication
 {
@@ -22,7 +24,68 @@ namespace ConsoleApplication
 
             Console.WriteLine("Hello World!");
 
+            
+
             return 0;
         }
+    }
+
+    public class Taests 
+    {
+        [Test]
+        public void thing() {
+
+            IProxyGenerator proxyGenerator = new ProxyGenerator();
+
+            IPersonRepository personRepo = proxyGenerator.CreateInterfaceProxyWithoutTarget<IPersonRepository>(new MyInterceptor<IPersonRepository>());
+
+            var person = new Person {
+                FirstName = "John",
+                LastName = "Smith",
+                Age = 56
+            };
+
+            string id = personRepo.Save(person);
+
+            Assert.False(string.IsNullOrEmpty(id), "Save() must return the Id of the object");
+
+            
+        }
+    }
+
+    public class Person 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(string id);
+        string Save(T obj);
+        T Delete(string id);
+    }
+
+    public class MyInterceptor<T> : IInterceptor
+    {
+        // public MyInterceptor(IRepository)
+        public void Intercept(IInvocation invocation)
+        {
+            switch ( invocation.Method.Name ) {
+                case "Save":
+                    Console.WriteLine("SAVE");
+                    break;
+                default:
+                    Console.WriteLine("unhandled");
+                    break;
+            }
+        }
+    }
+
+
+    public interface IPersonRepository : IRepository<Person> {
+
     }
 }
